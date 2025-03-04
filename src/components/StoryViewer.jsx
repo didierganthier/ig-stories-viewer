6bb04129fa2f7fbfe2edf4a091c5a55860d53ce7
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-03-04 07:16:04 -0500

    Change extension from ts to js, add .history to .gitignore and create basic skeleton for StoryViewer

---
+++ src/components/StoryViewer.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const StoryViewer = ({ story, onNext, onClose }) => {
+    const [progress, setProgress] = useState(0);
+
+    return (
+        <div></div>
+    )
+}
---
+++ src/data/index.js
@@ -0,0 +1,62 @@
+export const stories = [
+    {
+        id: 1,
+        image: 'https://randomuser.me/api/portraits/men/5.jpg',
+        storyImages: [
+            'https://th.bing.com/th/id/R.1ba9999686ddfbdba7fbf86157c39469?rik=8qE3fcnPPRcCqg&pid=ImgRaw&r=0',
+            'https://cdn.pixabay.com/photo/2016/09/07/11/37/sunset-1651426_1440_2560.jpg',
+            'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_1280.jpg',
+            'https://th.bing.com/th/id/R.6a57353462ce99eb1cf49ffdbfa9c04f?rik=4FAvad2ZCVMy0Q&pid=ImgRaw&r=0',
+            
+         ],
+        user: 'user1'
+    },
+    {
+        id: 2,
+        image: 'https://randomuser.me/api/portraits/women/2.jpg',
+        storyImages: [
+            'https://th.bing.com/th/id/R.1ba9999686ddfbdba7fbf86157c39469?rik=8qE3fcnPPRcCqg&pid=ImgRaw&r=0',
+            'https://cdn.pixabay.com/photo/2016/09/07/11/37/sunset-1651426_1440_2560.jpg',
+            'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_1280.jpg',
+            'https://th.bing.com/th/id/R.6a57353462ce99eb1cf49ffdbfa9c04f?rik=4FAvad2ZCVMy0Q&pid=ImgRaw&r=0',
+            'https://images.pexels.com/videos/856973/free-video-856973.jpg'
+         ],
+        user: 'user2'
+    },
+    {
+        id: 3,
+        image: 'https://randomuser.me/api/portraits/men/11.jpg',
+        storyImages: [
+            'https://th.bing.com/th/id/R.1ba9999686ddfbdba7fbf86157c39469?rik=8qE3fcnPPRcCqg&pid=ImgRaw&r=0',
+            'https://cdn.pixabay.com/photo/2016/09/07/11/37/sunset-1651426_1440_2560.jpg',
+            'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_1280.jpg',
+            'https://th.bing.com/th/id/R.6a57353462ce99eb1cf49ffdbfa9c04f?rik=4FAvad2ZCVMy0Q&pid=ImgRaw&r=0',
+            'https://images.pexels.com/videos/856973/free-video-856973.jpg'
+         ],
+        user: 'user3'
+    },
+    {
+        id: 4,
+        image: 'https://randomuser.me/api/portraits/women/15.jpg',
+        storyImages: [
+            'https://th.bing.com/th/id/R.1ba9999686ddfbdba7fbf86157c39469?rik=8qE3fcnPPRcCqg&pid=ImgRaw&r=0',
+            'https://cdn.pixabay.com/photo/2016/09/07/11/37/sunset-1651426_1440_2560.jpg',
+            'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_1280.jpg',
+            'https://th.bing.com/th/id/R.6a57353462ce99eb1cf49ffdbfa9c04f?rik=4FAvad2ZCVMy0Q&pid=ImgRaw&r=0',
+            'https://images.pexels.com/videos/856973/free-video-856973.jpg'
+         ],
+        user: 'user4'
+    },
+    {
+        id: 5,
+        image: 'https://randomuser.me/api/portraits/women/7.jpg',
+        storyImages: [
+            'https://th.bing.com/th/id/R.1ba9999686ddfbdba7fbf86157c39469?rik=8qE3fcnPPRcCqg&pid=ImgRaw&r=0',
+            'https://cdn.pixabay.com/photo/2016/09/07/11/37/sunset-1651426_1440_2560.jpg',
+            'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455_1280.jpg',
+            'https://th.bing.com/th/id/R.6a57353462ce99eb1cf49ffdbfa9c04f?rik=4FAvad2ZCVMy0Q&pid=ImgRaw&r=0',
+            'https://images.pexels.com/videos/856973/free-video-856973.jpg'
+         ],
+        user: 'user5'
+    }
+]
